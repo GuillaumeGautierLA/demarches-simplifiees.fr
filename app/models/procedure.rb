e0ff437cef96b5b3d838e6ec9459f110210cc2d6
Pierre de La Morinerie <kemenaran@gmail.com>
2020-08-05 18:42:35 +0200

    [WIP] zeitwerk: disable Array extension
    TODO: move it to `initializers/core_ext` instead

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -43,7 +43,8 @@
 #  published_revision_id               :bigint
 #  service_id                          :bigint
 #
-require Rails.root.join('lib', 'percentile')
+
+#require Rails.root.join('lib', 'percentile')
 
 class Procedure < ApplicationRecord
   self.ignored_columns = ['archived_at', 'csv_export_queued', 'xlsx_export_queued', 'ods_export_queued']
